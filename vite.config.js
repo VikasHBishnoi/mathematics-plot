ba8a83e2891feb3be640a79b04aa33966ea3e5f2
Vikas Bishnoi <vikasbishnoi992858@gmail.com>
2025-06-05 00:15:27 +0530

    Issue fix for deoployement url Fix url fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mathematics-plot/',
+  base: '/Mathematics-Plot/',
   plugins: [react()],
 })
